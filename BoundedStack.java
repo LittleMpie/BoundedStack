c6f9f65da9921cbb63e50c48ba8ef9ceac0d93e8
LittleMpie <runchida.sa@ku.th>
2026-07-30 18:53:34 +0700

    RI checkRep

--- BoundedStack.java
+++ BoundedStack.java
@@ -13,28 +13,68 @@ import java.util.*;
  * 
  */
 public class BoundedStack {
-    private final List<String> names ;
-    private final int number;
 
-    //AF(names,number) = รายชื่อคิวของลูกค้าที่ร้านชานมไข่มุก ตามลำดับ , หมายเลขคิวของลูกค้า
+
+    private final List<String> names ;
+    private int queue;
+    public static final int MAX_NAMES = 150;
+    private int head;        
+    private int nextQueue ; 
+    private final int capacity;
+
+
+
+
+
+
+    //AF(names, queue ,head, nextQueue , capacity ) = รายชื่อคิวของลูกค้าที่ร้านชานมไข่มุก ตามลำดับ , หมายเลขคิวของลูกค้าคนปัจจุบัน , หัวคิว  , เลขคิวที่วิ่งไปเรื่อย ๆ , ความจุของคิว
+
+
     //RI
     // - names != null
-    // - ชื่อลูกค้าต้องไม่เป็น String ว่าง
-    // - หมายเลขคิวต้อง ต้อง >0
-    // - หมายเลขคิวต้องไม่ซ้ำกัน
+    // - names != String ว่าง
+    // - names <= 20 ตัวอักษร 
+    // - queue > 0
+    // - 0 < capacity <= MAX_NAMES(150)
+    // - หมายเลขคิวต้องไม่ซ้ำกัน 
+    // - nextQueue > 0
+    // - 0 > head < capacity
 
     // Safety from rep exposure:
-    //   ให้ ืnames , number เป็น final
+    //   ให้ names , capacity เป็น final
     //   คัดลอกทั้งขาเข้าและขาออก
+
+    private void checkRep(){
+        assert names != null;
+        assert names.size() <= capacity ;
+
+
+        //Set<String> seen = new HashSet<>();
+        Set<String> seen = new LinkedHashSet<>();
+        
+        for(int i = 0 ; i<queue;i++){
+        String s = names.get((head + i) % capacity ) ;
+        assert s != null ;
+        assert !(s== "") ; 
+        assert seen.add(s) : "duplicate" + s;
+        }
+
+    }
+
+    
    
 
-    public BoundedStack( int number){
-
-
-
-
+    public BoundedStack( int capacity ){
+        
+        if (capacity <= 0 || capacity > MAX_NAMES) {
+            throw new IllegalArgumentException("Capacity must be between 1 and " + MAX_NAMES);
+        }
         this.names = new ArrayList<>();
-        this.number = number;
+        this.queue = 0; 
+        this.head = 0;
+        this.nextQueue = 1 ;
+        this.capacity = capacity ;
+        checkRep();
         
 
 
